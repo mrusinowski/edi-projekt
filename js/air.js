533f03dbb382c710ee5e482e265b7043af71d54c
ptabis <p.tabis@protonmail.com>
2020-01-15 12:36:47 +0100

    Obsługa api jakości powietrza

---
+++ js/air.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    function getAirPollution() {        
+        let cities = document.getElementById('cities')
+        let val = cities.options[cities.selectedIndex]
+        let lng = val.getAttribute('data-lng')
+        let lat = val.getAttribute('data-lat')
+        fetch('https://airapi.airly.eu/v2/measurements/nearest?lat='+lat+'&lng='+lng+'&maxDistanceKM=30', {
+            headers: {
+                'apikey': 'eVy2LvD5QyFeFzI0n5NFN8IqLePzwU8R'
+            }
+        })
+        .then((res) => {return res.json()})
+        .then((json) => {
+            if(json.current.indexes[0].description == "W tej okolicy nie ma jeszcze naszych sensorów.")
+            json.current.indexes[0].description = "Brak danych."
+            document.getElementById('air-span').innerHTML = json.current.indexes[0].description
+        })
+    }
+    document.getElementById('cities').onchange = () => {
+        getAirPollution()
+    }
+    getAirPollution()
+
+}
