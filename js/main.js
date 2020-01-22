f83d4fbd7a73a0a27d13909dd582a5adc81b399c
Michał <michalr101@gmail.com>
2020-01-22 23:41:45 +0100

    poprawa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     document.getElementById('cities').onchange = () => {
         getAirPollution()
+        drawPollutionChart()
         drawWeatherChart()
     }
     document.getElementById('pollution-type').onchange = () => {
