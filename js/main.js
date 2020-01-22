36c13724e3901425c3c1be726f6617f81e62a017
Michał <michalr101@gmail.com>
2020-01-22 22:44:29 +0100

    weather

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ window.onload = () => {
     document.getElementById('cities').onchange = () => {
         getAirPollution()
         drawPollutionChart()
+        drawWeatherChart()
     }
     document.getElementById('pollution-type').onchange = () => {
         drawPollutionChart()
     }
     getAirPollution()
     drawPollutionChart()
+    drawWeatherChart()
 }
