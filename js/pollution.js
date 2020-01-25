634661c12203ead0df79b8371e8000493810c675
Tryor1 <56543487+Tryor1@users.noreply.github.com>
2020-01-25 14:15:33 +0100

    Add files via upload

--- js/pollution.js
+++ js/pollution.js
@@ -2,7 +2,7 @@ var pollution_chart_config = {
     labels: [],
     series: [[]]
 }
-var chart = null
+var chart1 = null
 function drawPollutionChart() {
     pollution_chart_config.series = [[]]
     pollution_chart_config.labels = []
@@ -35,7 +35,33 @@ function drawPollutionChart() {
 
                 }
             }
-            chart = new Chartist.Line('.ct-chart', pollution_chart_config)
+            chart1 = new Chartist.Line('#pollution-chart', pollution_chart_config)
         })
 }
 
+var gold_chart_config = {
+    labels: [],
+    series: [[]]
+    }
+var chart2 = null
+var gold_options = {
+    high: 200,
+    low: 180,
+  };
+function drawgoldChart() {
+        gold_chart_config.series = [[]]
+        gold_chart_config.labels = []
+        let gold_price_days = document.getElementById('gold_days').value
+        fetch('http://api.nbp.pl/api/cenyzlota/last/' + gold_price_days)
+            .then((res) => { return res.json() })
+            .then((json) => {
+                let dat = json[1].cena
+                for (let i = 0; i < json.length; i++){
+                    let l = json[i].data
+                    let s = json[i].cena
+                    gold_chart_config.labels.push(l)
+                    gold_chart_config.series[0].push(s)
+                }
+                chart2 = new Chartist.Bar('#gold-chart', gold_chart_config, gold_options)
+            })
+}
